a20e7313781034d7930b408a0e958cc601beb5fc
JangDaeHoon <jub701@naver.com>
2024-11-27 09:15:50 +0900

    Character Script Awake메시지 함수 제거

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,25 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody), typeof(Collider), typeof(Animator))]
-public abstract class Character : MonoBehaviour
+namespace HoonsCodes
 {
-    [Header("Character Info")]
-    [SerializeField] protected float maxHp;
-    [SerializeField] protected float curHp;
-    [SerializeField] protected float moveSpeed;
+    [RequireComponent(typeof(Rigidbody), typeof(Collider), typeof(Animator))]
+    public abstract class Character : MonoBehaviour
+    {
+        [Header("Character Info")]
+        [SerializeField] protected float maxHp;
+        [SerializeField] protected float curHp;
+        [SerializeField] protected float moveSpeed;
 
-    [SerializeField] protected float dmgValue;
-    [SerializeField] protected float atkSpeed;   //공격속도
+        [SerializeField] protected float dmgValue;
+        [SerializeField] protected float atkSpeed;   //공격속도
 
-    [SerializeField] protected bool isDead;
-    [Header("----------------------------")]
-    protected Rigidbody rb;
-    protected Collider col;
-    protected Animator animator;
+        [SerializeField] protected bool isDead;
+        [Header("----------------------------")]
+        protected Rigidbody rb;
+        protected Collider col;
+        protected Animator animator;
 
-    //정의 필수.
-    public abstract void Move();
-    public abstract void Dead();
+        //정의 필수.
+        public abstract void Move();
+        public abstract void Dead();
 
+    }
 }
