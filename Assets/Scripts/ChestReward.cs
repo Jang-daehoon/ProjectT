6213a83fa65424482c6b5c3a546e2925bcb29ded
KimMinJun <kmj387100@gmail.com>
2024-12-05 10:04:49 +0900

    2

--- Assets/Scripts/ChestReward.cs
+++ Assets/Scripts/ChestReward.cs
@@ -7,7 +7,6 @@ public class ChestReward : MonoBehaviour
 {
     public Animator animator;
     public bool getReward;  //보상 획득 여부
-    private void Awake() => animator = GetComponentInChildren<Animator>();
     [Header("아르카나 데이터")]
     public bool isOpen;   //상자 열렸는지 확인
 
