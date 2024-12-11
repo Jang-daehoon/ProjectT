ac62cf296a99005cc13dd556e8b66290c0e1bd4b
nul <qjarns1423@gmail.com>
2024-12-11 17:46:15 +0900

    퍼즈 메뉴 간단하게 만듬

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // 다른 스크립트에서 쉽게 접근이 가능하도록 static
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuCanvas;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused == true)
+                Resume();
+            else
+                Pause();
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuCanvas.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    public void Pause()
+    {
+        pauseMenuCanvas.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
