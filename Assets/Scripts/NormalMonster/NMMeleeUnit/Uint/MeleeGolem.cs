8e0059c50a0a12a6b60eb327803215dd1d540bae
KimMinJun <kmj387100@gmail.com>
2024-12-12 13:09:38 +0900

    골렘 수정

--- Assets/Scripts/NormalMonster/NMMeleeUnit/Uint/MeleeGolem.cs
+++ Assets/Scripts/NormalMonster/NMMeleeUnit/Uint/MeleeGolem.cs
@@ -10,6 +10,7 @@ public class MeleeGolem : NMMeleeUnit
     private bool isGolemAttack = false;
     private bool isHit = false;
     private bool isGolemAttackCollTime = false;
+    private bool deadanim = false;
 
     public UnitRange attackRange;
     public ParticleSystem particle;
@@ -32,9 +33,10 @@ public class MeleeGolem : NMMeleeUnit
     protected override void Update()
     {
         HpBarUpdate();
-        if (curHp <= 0 && isDead == false)//죽을때 한번 발동
+        if (curHp <= 0 && deadanim == false)//죽을때 한번 발동
         {
-            isDead = true; 
+            deadanim = true;
+            isDead = true;
             StopAllCoroutines();
             ResetAnimatorBools(); // 모든 애니메이션 초기화
             animator.SetTrigger("Die");
@@ -72,12 +74,6 @@ public class MeleeGolem : NMMeleeUnit
 
         yield return new WaitForSeconds(1f);//돌진시작
 
-        if (isDead == true)
-        {
-            ResetAnimatorBools();
-            yield break; //죽었으면 중단
-        }
-
         col.isTrigger = true;
         isGolemAttack = true;
         attackRange.gameObject.SetActive(false);//이동범위 표시 Off
@@ -107,7 +103,7 @@ public class MeleeGolem : NMMeleeUnit
         col.isTrigger = false;
         isGolemAttack = false;
         attackRange.transform.position = this.transform.position;
-        if (isDead == true) animator.SetBool("Attack", false);
+        animator.SetBool("Attack", false);
         particle.gameObject.SetActive(false);
         particle.Stop();
         isGolemAttackCollTime = true;
@@ -140,5 +136,6 @@ public class MeleeGolem : NMMeleeUnit
         animator.SetBool("Idel", false);
         animator.SetBool("Attack", false);
         animator.Update(0); //애니메이터 상태 강제 동기화
+        animator.SetTrigger("Die");
     }
 }
